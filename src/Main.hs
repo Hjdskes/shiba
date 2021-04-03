c21f798f652e8fb09a0196d73457d34124720b73
Jente Hidskes <dev@hjdskes.nl>
2021-04-03 17:23:34 +0200

    Scrape first page

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,22 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module Main where
 
+import Text.HTML.Scalpel
+import Text.StringLike (StringLike)
+
+besok :: Scraper String String
+besok = text $ "div" @: [hasClass "entry-content"]
+
+config :: StringLike str => Config str
+config = Config utf8Decoder Nothing
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let url = "https://blog.sutamuroku.com/besok/"
+  scrapeURLWithConfig config url besok >>= \case
+    Just scraped -> do
+      putStrLn $ "Scraped " <> url
+    Nothing -> putStrLn $ "Failed to scrape " <> url
