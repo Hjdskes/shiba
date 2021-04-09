f37da00061727bfe06b5f9fd84fffcaccddd7fa9
Jente Hidskes <dev@hjdskes.nl>
2021-04-09 20:27:40 +0200

    Move definition of scrape targets to Scraper.hs

--- src/Scraper/Shiba.hs
+++ src/Scraper/Shiba.hs
@@ -2,15 +2,12 @@ module Scraper.Shiba
   ( scrape
   ) where
 
+import Data.Text         (Text, unpack)
 import Text.HTML.Scalpel hiding (scrape)
 import Text.StringLike   (StringLike)
-
-besok :: Scraper String String
-besok = text $ "div" @: [hasClass "entry-content"]
 
 config :: StringLike str => Config str
 config = Config utf8Decoder Nothing
 
-scrape :: IO (Maybe String)
-scrape = scrapeURLWithConfig config url besok
-  where url = "https://blog.sutamuroku.com/besok/"
+scrape :: StringLike str => Text -> Scraper str a -> IO (Maybe a)
+scrape url = scrapeURLWithConfig config (unpack url)
