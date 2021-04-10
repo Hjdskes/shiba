703402af4dc5ef546fbbb5efcb31a9dffa647cc6
Jente Hidskes <dev@hjdskes.nl>
2021-04-10 14:26:33 +0200

    Make scrape function tagless

--- src/Scraper/Shiba.hs
+++ src/Scraper/Shiba.hs
@@ -2,12 +2,13 @@ module Scraper.Shiba
   ( scrape
   ) where
 
-import Data.Text         (Text, unpack)
-import Text.HTML.Scalpel hiding (scrape)
-import Text.StringLike   (StringLike)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Data.Text              (Text, unpack)
+import Text.HTML.Scalpel      hiding (scrape)
+import Text.StringLike        (StringLike)
 
 config :: StringLike str => Config str
 config = Config utf8Decoder Nothing
 
-scrape :: StringLike str => Text -> Scraper str a -> IO (Maybe a)
-scrape url = scrapeURLWithConfig config (unpack url)
+scrape :: MonadIO m => StringLike str => Text -> Scraper str a -> m (Maybe a)
+scrape url = liftIO . scrapeURLWithConfig config (unpack url)
