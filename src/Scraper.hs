4da6460cc5dbbffecac74263f5b06927a1a464fc
Jente Hidskes <dev@hjdskes.nl>
2021-04-10 17:31:28 +0200

    Replace separate persist and retrieve with a conditional persist

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -12,13 +12,18 @@ import           Control.Monad.Trans.AWS      (AWST', HasEnv, LogLevel (..),
                                                envLogger, newEnv, newLogger,
                                                runAWST, runResourceT, send)
 import           Control.Monad.Trans.Resource (MonadUnliftIO, ResourceT)
-import qualified Data.HashMap.Strict          as HashMap (fromList, lookup)
+import qualified Data.HashMap.Strict          as HashMap (fromList, lookup,
+                                                          null)
 import           Data.IORef                   (readIORef)
-import           Data.Text                    (Text, pack, unpack)
+import           Data.Text                    (Text, pack)
 import           Network.AWS                  (Credentials (Discover), Env)
-import           Network.AWS.DynamoDB         (attributeValue, avS, getItem,
-                                               giKey, girsItem, piItem, putItem)
-import           Network.AWS.DynamoDB.PutItem (PutItemResponse)
+import           Network.AWS.DynamoDB         (attributeValue, avS,
+                                               piConditionExpression,
+                                               piExpressionAttributeValues,
+                                               piItem, piReturnValues,
+                                               pirsAttributes,
+                                               pirsResponseStatus, putItem)
+import           Network.AWS.DynamoDB.Types   (ReturnValue (AllOld))
 import           Scraper.Shiba
 import           System.IO                    (stdout)
 import           Text.HTML.Scalpel            (Scraper, hasClass, text, (@:))
@@ -26,19 +31,27 @@ import           Text.HTML.Scalpel            (Scraper, hasClass, text, (@:))
 withDynamoDB :: HasEnv r => MonadUnliftIO m => r -> AWST' r (ResourceT m) a -> m a
 withDynamoDB env action = runResourceT . runAWST env $ action
 
-persist :: MonadCatch m => MonadUnliftIO m => AppConfig -> Text -> Text -> m PutItemResponse
-persist AppConfig{..} key value = withDynamoDB env $
-  send $ putItem tableName & piItem .~ item
-  where item = HashMap.fromList
-          [ ("website", attributeValue & avS ?~ key)
-          , ("scraped", attributeValue & avS ?~ value)
-          ]
+data PersistenceResult a = ItemInserted | ItemUpdated a | Failed
 
-retrieve :: MonadCatch m => MonadUnliftIO m => AppConfig -> Text -> m (Maybe Text)
-retrieve AppConfig{..} key = withDynamoDB env $ do
-  result <- send $ getItem tableName & giKey .~ key'
-  return $ HashMap.lookup "scraped" (result ^. girsItem) >>= \m -> m ^. avS
-  where key' = HashMap.fromList [ ("website", attributeValue & (avS ?~ key)) ]
+-- TODO: typeclass to make item?
+persist :: MonadCatch m => MonadUnliftIO m => AppConfig -> Text -> Text -> m (PersistenceResult (Maybe Text))
+persist AppConfig{..} key value = withDynamoDB env $ processResponse <$> send request
+  where
+    item = HashMap.fromList [ ("website", attributeValue & avS ?~ key), ("scraped", attributeValue & avS ?~ value) ]
+    expressionAttributeValues = HashMap.fromList [ (":scraped", attributeValue & avS ?~ value) ]
+    conditionExpression = "scraped <> :scraped"
+    request = putItem tableName & piItem .~ item
+      & piReturnValues ?~ AllOld
+      & piExpressionAttributeValues .~ expressionAttributeValues
+      & piConditionExpression ?~ conditionExpression
+    processResponse response =
+      if (response ^. pirsResponseStatus) == 200
+        then
+          let returnValues = response ^. pirsAttributes
+          in if (HashMap.null returnValues)
+            then ItemInserted
+            else ItemUpdated (HashMap.lookup "scraped" returnValues >>= \m -> m ^. avS)
+        else Failed
 
 data AppConfig = AppConfig
   { env       :: Env
@@ -71,22 +84,12 @@ checkForChange :: MonadCatch m => MonadUnliftIO m => AppConfig -> ScrapeTarget S
 checkForChange appConfig ScrapeTarget{..} =
   fmap pack <$> scrape url scraper >>= \case
     Just scraped -> do
-      -- putStrLn $ "Scraped " <> unpack scraped
-      retrieve appConfig url >>= \case
-        Nothing -> do
-          -- putStrLn $ "No previous scrape result for " <> unpack url <> ", persisting new"
-          _ <- persist appConfig url scraped
-          return $ Right ()
-        Just previous | previous == scraped -> do
-          -- putStrLn $ "Previous scrape result for " <> unpack url <> " is identical to current, ignoring"
-          return $ Right ()
-        Just _ -> do
-          -- putStrLn $ "Previous scrape result is different for " <> unpack url <> ", storing and notifying"
-          _ <- persist appConfig url scraped
-          return $ Right ()
-    Nothing -> do
-      -- putStrLn "Failed to scrape"
-      return $ Left "Failed to scrape"
+      res <- persist appConfig url scraped
+      case res of
+        Failed        -> return $ Left "Failed to insert or update DynamoDB"
+        ItemInserted  -> return $ Right ()
+        ItemUpdated _ -> return $ Right ()
+    Nothing -> return $ Left "Failed to scrape"
 
 handler :: String -> Context AppConfig -> IO (Either String ())
 handler _request context = do
