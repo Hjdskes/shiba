ab91ef239ef657e34492c79299f6ee318b0dca46
Jente Hidskes <dev@hjdskes.nl>
2021-04-13 19:35:24 +0200

    Use GetItem+PutItem rather than conditional UpdateItem
    A condition expression evaluation failure returns a 400 Bad Request
    which leads to an exception.

--- src/DynamoDB.hs
+++ src/DynamoDB.hs
@@ -5,42 +5,46 @@ module DynamoDB
 
 import           Config                       (AppConfig (..))
 import           Control.Lens                 ((&), (.~), (?~), (^.))
+import           Control.Monad                (void)
 import           Control.Monad.Catch          (MonadCatch)
 import           Control.Monad.Trans.AWS      (AWST', HasEnv, runAWST,
                                                runResourceT, send)
 import           Control.Monad.Trans.Resource (MonadUnliftIO, ResourceT)
-import qualified Data.HashMap.Strict          as HashMap (fromList, lookup,
-                                                          null)
+import           Data.HashMap.Strict          (HashMap)
+import qualified Data.HashMap.Strict          as HashMap (fromList, insert,
+                                                          lookup)
 import           Data.Maybe                   (fromJust)
 import           Data.Text                    (Text)
-import           Network.AWS.DynamoDB         (attributeValue, avS,
-                                               piConditionExpression,
-                                               piExpressionAttributeValues,
-                                               piItem, piReturnValues,
-                                               pirsAttributes,
-                                               pirsResponseStatus, putItem)
-import           Network.AWS.DynamoDB.Types   (ReturnValue (AllOld))
+import           Network.AWS.DynamoDB         (attributeValue, avS, getItem,
+                                               giKey, girsItem, piItem, putItem)
+import           Network.AWS.DynamoDB.Types   (AttributeValue)
 
 withDynamoDB :: HasEnv r => MonadUnliftIO m => r -> AWST' r (ResourceT m) a -> m a
 withDynamoDB env = runResourceT . runAWST env
 
-data UpsertResult a = ItemInsertedOrUnchanged a | ItemUpdated a | Failed Int
+get :: MonadCatch m => MonadUnliftIO m => AppConfig -> HashMap Text AttributeValue -> m (Maybe Text)
+get AppConfig{..} key = withDynamoDB env $ do
+  result <- send $ getItem tableName & giKey .~ key
+  return $ (^. avS) <$> fromJust $ HashMap.lookup "scraped" (result ^. girsItem)
+
+set :: MonadCatch m => MonadUnliftIO m => AppConfig -> HashMap Text AttributeValue -> m ()
+set AppConfig{..} item = withDynamoDB env $
+  void $ send $ putItem tableName & piItem .~ item
+
+data UpsertResult a = ItemInserted a | ItemUnchanged a | ItemUpdated a
 
 upsert :: MonadCatch m => MonadUnliftIO m => AppConfig -> Text -> Text -> m (UpsertResult Text)
-upsert AppConfig{..} key value = withDynamoDB env $ processResponse <$> send request
+upsert config key value = do
+  prev <- get config key'
+  case prev of
+    Just p | p == value ->
+      return $ ItemUnchanged p
+    Just _ -> do
+      _ <- set config item
+      return $ ItemUpdated value
+    Nothing -> do
+      _ <- set config item
+      return $ ItemInserted value
   where
-    item = HashMap.fromList [ ("website", attributeValue & avS ?~ key), ("scraped", attributeValue & avS ?~ value) ]
-    expressionAttributeValues = HashMap.fromList [ (":scraped", attributeValue & avS ?~ value) ]
-    conditionExpression = "scraped <> :scraped"
-    request = putItem tableName & piItem .~ item
-      & piReturnValues ?~ AllOld
-      & piExpressionAttributeValues .~ expressionAttributeValues
-      & piConditionExpression ?~ conditionExpression
-    processResponse response =
-      if (response ^. pirsResponseStatus) == 200
-        then
-          let returnValues = response ^. pirsAttributes
-          in if HashMap.null returnValues
-            then ItemInsertedOrUnchanged value
-            else ItemUpdated (fromJust $ HashMap.lookup "scraped" returnValues >>= \m -> m ^. avS)
-        else Failed (response ^. pirsResponseStatus)
+    key' = HashMap.fromList [ ("website", attributeValue & avS ?~ key) ]
+    item = HashMap.insert "scraped" (attributeValue & avS ?~ value) key'
