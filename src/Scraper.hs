cc0068bae826580b8f5221519b7de6732929e088
Jente Hidskes <dev@hjdskes.nl>
2021-04-17 01:40:53 +0200

    Extract and refactor main logic
    This will make it easier to iterate over multiple scrape targets, and
    runs the entire persistence and notifying chain in a single runAWST.

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -3,16 +3,17 @@ module Scraper
   ) where
 
 import Aws.Lambda
-import Config                       (AppConfig (..))
-import Control.Monad.Trans.AWS      (runAWST, runResourceT)
-import Data.Aeson                   (Object)
-import Data.IORef                   (readIORef)
-import Data.Text                    (Text)
-import DynamoDB                     (UpsertResult (..), upsert)
-import Network.AWS                  (MonadAWS, liftAWS)
-import SNS                          (notify)
-import Scrape                       (scrape)
-import Text.HTML.Scalpel            (Scraper, hasClass, text, (@:))
+import Config                  (AppConfig (..))
+import Control.Monad.Trans.AWS (runAWST, runResourceT)
+import Data.Aeson              (Object)
+import Data.Either.Combinators (maybeToRight)
+import Data.IORef              (readIORef)
+import Data.Text               (Text)
+import DynamoDB                (UpsertResult (..), upsert)
+import Network.AWS             (MonadAWS, liftAWS)
+import SNS                     (notify)
+import Scrape                  (scrape)
+import Text.HTML.Scalpel       (Scraper, hasClass, text, (@:))
 
 -- | A grouping of a url to scrape and a scraper to execute on its page.
 data ScrapeTarget str a = ScrapeTarget
@@ -31,31 +32,29 @@ scrapeTarget =
 
 data ScrapeResult url new = NoChange url | TargetChanged url new
 
-checkForChange :: MonadAWS m => ScrapeTarget Text Text -> m (Either String (ScrapeResult Text Text))
-checkForChange ScrapeTarget{..} =
-  -- TODO: deal with exceptions
-  scrape url scraper >>= \case
-    Just scraped -> do
-      -- TODO: deal with exceptions
-      res <- liftAWS $ upsert url scraped
-      return $ case res of
-        ItemUpdated item  -> Right $ TargetChanged url item
-        ItemInserted item -> Right $ TargetChanged url item
-        ItemUnchanged _   -> Right $ NoChange url
-    Nothing -> return $ Left "Failed to scrape"
+-- TODO: deal with exceptions
+check :: Traversable t => MonadAWS m => Text -> t Text -> m (t (ScrapeResult Text Text))
+check url = mapM (\scraped -> toScrapeResult <$> liftAWS (upsert url scraped))
+  where
+    toScrapeResult (ItemInserted item) = TargetChanged url item
+    toScrapeResult (ItemUpdated item)  = TargetChanged url item
+    toScrapeResult (ItemUnchanged _)   = NoChange url
 
+-- TODO: deal with exceptions
 sendSms :: MonadAWS m => ScrapeResult Text a -> m ()
 sendSms (NoChange _) = pure ()
 sendSms (TargetChanged url _) = mapM_ (liftAWS . notify message) phoneNumbers
   where message = url <> " has changed. Press the link to take a look!"
         phoneNumbers = [ "+46704350740", "+31624364852" ]
 
+main :: MonadAWS m => ScrapeTarget Text Text -> m (Maybe ())
+main ScrapeTarget{..} = do
+  scraped <- scrape url scraper
+  scrapeResult <- check url scraped
+  mapM sendSms scrapeResult
+
 handler :: Object -> Context AppConfig -> IO (Either String ())
 handler _request context = do
   appConfig <- readIORef $ customContext context
-  scrapeResult <- runResourceT . runAWST (env appConfig) $ checkForChange scrapeTarget
-  case scrapeResult of
-    Left err  -> return $ Left err
-    Right res -> do
-      _ <- runResourceT . runAWST (env appConfig) $ sendSms res
-      return $ Right ()
+  result <- runResourceT . runAWST (env appConfig) $ main scrapeTarget
+  return $ maybeToRight "Failed to scrape" result
