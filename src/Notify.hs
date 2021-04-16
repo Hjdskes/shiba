77c5ce08074ccd8c2682c3d6b40a08b749a214ba
Jente Hidskes <dev@hjdskes.nl>
2021-04-17 01:03:27 +0200

    Make Notify module internals use MonadAWS
    This allows us to disentagle AppConfig and simplify the internal
    transformer stack.

--- src/Notify.hs
+++ src/Notify.hs
@@ -2,18 +2,13 @@ module Notify
   ( notify
   ) where
 
-import Config                       (AppConfig (..))
-import Control.Lens                 ((&), (?~))
-import Control.Monad                (void)
-import Control.Monad.Catch          (MonadCatch)
-import Control.Monad.Trans.AWS      (AWST', HasEnv, runAWST, runResourceT, send)
-import Control.Monad.Trans.Resource (MonadUnliftIO, ResourceT)
-import Data.Text                    (Text)
-import Network.AWS.SNS.Publish      (pPhoneNumber, publish)
+import Control.Lens            ((&), (?~))
+import Control.Monad           (void)
+import Control.Monad.Trans.AWS (send)
+import Data.Text               (Text)
+import Network.AWS             (MonadAWS, liftAWS)
+import Network.AWS.SNS.Publish (pPhoneNumber, publish)
 
-withSns :: HasEnv r => MonadUnliftIO m => r -> AWST' r (ResourceT m) a -> m a
-withSns env = runResourceT . runAWST env
-
-notify :: MonadCatch m => MonadUnliftIO m => AppConfig -> Text -> Text -> m ()
-notify AppConfig{..} message phoneNumber = withSns env $
+notify :: MonadAWS m => Text -> Text -> m ()
+notify message phoneNumber = liftAWS $
   void $ send $ publish message & pPhoneNumber ?~ phoneNumber
