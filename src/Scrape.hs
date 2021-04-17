c5495649c7ca3a68d90917b954dc232b8a8d6f9f
Jente Hidskes <dev@hjdskes.nl>
2021-04-17 16:16:26 +0200

    Use defaultConfig with defaultDecoder
    While the first scraping target didn't return the `Content-Type` header
    and returned UTF-8, the two other targets do return that header and
    return Latin-1. Hence, we just rely on the default config and the first
    scraping target's results include some gibberish. This is fine since we
    only use the results for comparison anyway; they are never displayed to
    the user.

--- src/Scrape.hs
+++ src/Scrape.hs
@@ -7,8 +7,5 @@ import Data.Text              (Text, unpack)
 import Text.HTML.Scalpel      hiding (scrape)
 import Text.StringLike        (StringLike)
 
-config :: StringLike str => Config str
-config = Config utf8Decoder Nothing
-
 scrape :: MonadIO m => StringLike str => Text -> Scraper str a -> m (Maybe a)
-scrape url = liftIO . scrapeURLWithConfig config (unpack url)
+scrape url = liftIO . scrapeURL (unpack url)
