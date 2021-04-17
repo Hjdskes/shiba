7fa81b157293aacce2a9b650f1f2618b1730f90e
Jente Hidskes <dev@hjdskes.nl>
2021-04-17 02:01:02 +0200

    Rename sendSms to notify

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -2,18 +2,18 @@ module Scraper
   ( handler
   ) where
 
-import Aws.Lambda
-import Config                  (AppConfig (..))
-import Control.Monad.Trans.AWS (runAWST, runResourceT)
-import Data.Aeson              (Object)
-import Data.Either.Combinators (maybeToRight)
-import Data.IORef              (readIORef)
-import Data.Text               (Text)
-import DynamoDB                (UpsertResult (..), upsert)
-import Network.AWS             (MonadAWS, liftAWS)
-import SNS                     (notify)
-import Scrape                  (scrape)
-import Text.HTML.Scalpel       (Scraper, hasClass, text, (@:))
+import           Aws.Lambda
+import           Config                  (AppConfig (..))
+import           Control.Monad.Trans.AWS (runAWST, runResourceT)
+import           Data.Aeson              (Object)
+import           Data.Either.Combinators (maybeToRight)
+import           Data.IORef              (readIORef)
+import           Data.Text               (Text)
+import           DynamoDB                (UpsertResult (..), upsert)
+import           Network.AWS             (MonadAWS, liftAWS)
+import qualified SNS                     (notify)
+import           Scrape                  (scrape)
+import           Text.HTML.Scalpel       (Scraper, hasClass, text, (@:))
 
 -- | A grouping of a url to scrape and a scraper to execute on its page.
 data ScrapeTarget str a = ScrapeTarget
@@ -41,9 +41,9 @@ check url = mapM (\scraped -> toScrapeResult <$> liftAWS (upsert url scraped))
     toScrapeResult (ItemUnchanged _)   = NoChange url
 
 -- TODO: deal with exceptions
-sendSms :: MonadAWS m => ScrapeResult Text a -> m ()
-sendSms (NoChange _) = pure ()
-sendSms (TargetChanged url _) = mapM_ (liftAWS . notify message) phoneNumbers
+notify :: MonadAWS m => ScrapeResult Text a -> m ()
+notify (NoChange _) = pure ()
+notify (TargetChanged url _) = mapM_ (liftAWS . SNS.notify message) phoneNumbers
   where message = url <> " has changed. Press the link to take a look!"
         phoneNumbers = [ "+46704350740", "+31624364852" ]
 
@@ -51,7 +51,7 @@ main :: MonadAWS m => ScrapeTarget Text Text -> m (Maybe ())
 main ScrapeTarget{..} = do
   scraped <- scrape url scraper
   scrapeResult <- check url scraped
-  mapM sendSms scrapeResult
+  mapM notify scrapeResult
 
 handler :: Object -> Context AppConfig -> IO (Either String ())
 handler _request context = do
