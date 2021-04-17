a387cc957ca4a295578d254b612340ea01124e61
Jente Hidskes <dev@hjdskes.nl>
2021-04-17 12:47:07 +0200

    Handle AWS exceptions

--- src/Scraper.hs
+++ src/Scraper.hs
@@ -4,13 +4,14 @@ module Scraper
 
 import           Aws.Lambda
 import           Config                  (AppConfig (..))
-import           Control.Monad.Trans.AWS (runAWST, runResourceT)
+import           Control.Exception.Lens  (handling)
+import           Control.Monad.Trans.AWS (_Error, runAWST, runResourceT)
 import           Data.Aeson              (Object)
 import           Data.Either.Combinators (maybeToRight)
 import           Data.IORef              (readIORef)
 import           Data.Text               (Text)
 import           DynamoDB                (UpsertResult (..), upsert)
-import           Network.AWS             (MonadAWS, liftAWS)
+import           Network.AWS             (Error, MonadAWS, liftAWS)
 import qualified SNS                     (notify)
 import           Scrape                  (scrape)
 import           Text.HTML.Scalpel       (Scraper, hasClass, text, (@:))
@@ -32,7 +33,6 @@ scrapeTarget =
 
 data ScrapeResult url new = NoChange url | TargetChanged url new
 
--- TODO: deal with exceptions
 check :: Traversable t => MonadAWS m => Text -> t Text -> m (t (ScrapeResult Text Text))
 check url = mapM (\scraped -> toScrapeResult <$> liftAWS (upsert url scraped))
   where
@@ -40,21 +40,22 @@ check url = mapM (\scraped -> toScrapeResult <$> liftAWS (upsert url scraped))
     toScrapeResult (ItemUpdated item)  = TargetChanged url item
     toScrapeResult (ItemUnchanged _)   = NoChange url
 
--- TODO: deal with exceptions
 notify :: MonadAWS m => ScrapeResult Text a -> m ()
 notify (NoChange _) = pure ()
 notify (TargetChanged url _) = mapM_ (liftAWS . SNS.notify message) phoneNumbers
   where message = url <> " has changed. Press the link to take a look!"
         phoneNumbers = [ "+46704350740", "+31624364852" ]
 
-main :: MonadAWS m => ScrapeTarget Text Text -> m (Maybe ())
-main ScrapeTarget{..} = do
-  scraped <- scrape url scraper
+main :: MonadAWS m => ScrapeTarget Text Text -> m (Either String ())
+main ScrapeTarget{..} = handling _Error errorToString $ do
+  scraped <- maybeToRight ("Failed to scrape " <> show url) <$> scrape url scraper
   scrapeResult <- check url scraped
   mapM notify scrapeResult
+  where
+    errorToString :: Applicative m => Error -> m (Either String ())
+    errorToString e = pure . Left $ "AWS exception scraping " <> show url <> ": " <> show e
 
 handler :: Object -> Context AppConfig -> IO (Either String ())
 handler _request context = do
   appConfig <- readIORef $ customContext context
-  result <- runResourceT . runAWST (env appConfig) $ main scrapeTarget
-  return $ maybeToRight "Failed to scrape" result
+  runResourceT . runAWST (env appConfig) $ main scrapeTarget
